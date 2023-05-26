e6338f2cba9d65f0b7a862bd2ee95230040e5741
XBai <xiaoBai@qq.com>
2023-05-26 13:01:25 +0800

    hot-fix 修改

--- src/main/java/com/atxbai/git/controller/userController.java
+++ src/main/java/com/atxbai/git/controller/userController.java
@@ -8,7 +8,8 @@ package com.atxbai.git.controller;
 public class userController {
     public static void main(String[] args) {
         System.out.println("hello.git");
-        System.out.println("测试提交");
+        System.out.println("测试提交2");
         System.out.println("测试提交3");
+        System.out.println("hot-fix 修复代码");
     }
 }
